3b6d15a416f199b5fa680ba71cefc66638411929
seojohann <seojohann@gmail.com>
2017-07-17 00:49:52 -0700

    practiced @RequestParam

--- springmvc/src/main/java/com/jsbomb/springmvc/controllers/BoardController.java
+++ springmvc/src/main/java/com/jsbomb/springmvc/controllers/BoardController.java
@@ -1,21 +1,26 @@
 package com.jsbomb.springmvc.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jsbomb.springmvc.model.MemberModel;
 
 @Controller
-@RequestMapping(value="/board/*")
+@RequestMapping(value="/board")
 public class BoardController {
 
-	@RequestMapping(value={"/board", "/board/view"}, method = RequestMethod.GET)
+	@RequestMapping(value={"/view"}, method = RequestMethod.GET)
 	public String boardView() {
 		
 		return "view";
 	}
 	
-	@RequestMapping(value="/board/content")
+	@RequestMapping(value="/content")
 	public String boardContent(Model model) {
 		
 		model.addAttribute("content", "this is the content");
@@ -23,9 +28,38 @@ public class BoardController {
 		return "content";
 	}
 	
-	@RequestMapping(value="/board/reply")
+	@RequestMapping(value="/reply")
 	public String boardReply(Model model) {
 		
 		return "reply";
+	}
+	
+	@RequestMapping(value="/getUserInfo")
+	public String getUserInfo(HttpServletRequest servletReq, Model model) {
+		
+		String id = servletReq.getParameter("id");
+		String password = servletReq.getParameter("password");
+		
+		model.addAttribute("id", id);
+		model.addAttribute("password", password);
+		
+		
+		return "getUserInfo";
+	}
+	
+	@RequestMapping(value="/getUserInfo2")
+	public String getUserInfo2(@RequestParam("id") String id,
+			@RequestParam("password") String password, Model model) {
+		
+		model.addAttribute("id", id);
+		model.addAttribute("password", password);
+		
+		return "getUserInfo";
+	}
+	
+	@RequestMapping("/detailedUser")
+	public String getUserInfo3(MemberModel memberModel) {
+		
+		return "detailedUser";
 	}
 }
---
+++ springmvc/src/main/java/com/jsbomb/springmvc/model/MemberModel.java
@@ -0,0 +1,36 @@
+package com.jsbomb.springmvc.model;
+
+public class MemberModel {
+
+	private String name;
+	private String dob;
+	private String id;
+	private String pw;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	
+}
